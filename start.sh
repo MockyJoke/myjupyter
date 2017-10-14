1fc7fffdff78c7130409c7d76fed183260afaa56
Pan <jnpan@8c85902e41ec.ant.amazon.com>
2017-10-14 02:30:30 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/root/Anaconda/bin/jupyter-notebook --allow-root --ip='0.0.0.0' --notebook-dir=/notebooks --no-browser --NotebookApp.token="$1"
+/root/Anaconda/bin/jupyter-notebook --allow-root --ip='0.0.0.0' --notebook-dir=/notebooks --no-browser --NotebookApp.token="$1" 
